b29f4c30f0fd7d2b0a1d872cb4f9d5b61d85de54
Lwulili <3420270455@qq.com>
2024-11-11 21:35:12 +0800

    project0

---
+++ proj0/Planet.java
@@ -0,0 +1,39 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    static final double G = 6.67e-11;
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        double dx = p.xxPos - xxPos;
+        double dy = p.yyPos - yyPos;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = calcDistance(p);
+        return (G * mass * p.mass) / (r * r);
+    }
+    
+}
